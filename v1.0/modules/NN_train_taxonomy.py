5694dddd35c1e6541eb3693da267fe9bc91c6822
Sirrah91 <92817370+Sirrah91@users.noreply.github.com>
2022-04-19 14:57:37 +0300

    Add files via upload

---
+++ v1.0/modules/NN_train_taxonomy.py
@@ -0,0 +1,146 @@
+from typing import Tuple, Union, Dict
+import numpy as np
+from datetime import datetime
+import talos
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Conv1D, MaxPooling1D, Flatten
+from keras.callbacks import EarlyStopping, History, TerminateOnNaN
+from keras import regularizers
+from pprint import pprint
+# from keras_visualizer import visualizer
+
+from modules.control_plots import plot_model_history
+from modules.NN_config_taxonomy import *
+from modules.utilities import *
+
+
+def FC_model(x_train: np.ndarray, params: Dict) -> Sequential:
+    #
+    # ADD BATCH NORMALISATION?
+    #
+
+    model = Sequential()
+    # Adding the input layer to the model
+    model.add(Dropout(params['dropout_input'], input_shape=(x_train.shape[1],)))
+
+    # Adding the hidden layers
+    for i in range(params['n_layers']):
+        model.add(Dense(params['n_nodes'][i], activation=params['input_activation'],
+                        kernel_regularizer=regularizers.l1(l1=params['lambda1'])))
+        # Dropout layer for stabilisation of the network
+        model.add(Dropout(params['dropout_hidden']))
+
+    # Number of nodes in output layer (same as classes)
+    model.add(Dense(num_labels, activation=params['output_activation']))
+
+    # Compiling the model to train. Declare the loss function and the optimizer (SGD, Adam, etc.)
+    model.compile(loss=loss, optimizer=params['optimizer'](learning_rate=params['learning_rate']),
+                  metrics=metrics)
+
+    return model
+
+
+def CNN_model(x_train: np.ndarray, params: Dict) -> Sequential:
+    # input_shape = (params['batch_size'], np.shape(x_train)[1], 1)
+    input_shape = (np.shape(x_train)[1], 1)
+
+    model = Sequential()
+    # Adding the input layer to the model
+    model.add(Dropout(params['dropout_input'], input_shape=input_shape))
+
+    # Adding the hidden layers
+    for i in range(params['n_layers']):
+        model.add(Conv1D(filters=params['n_nodes'][i], kernel_size=params['kern_size'],
+                         activation=params['input_activation'], padding='same'))
+        model.add(MaxPooling1D(pool_size=2))
+        model.add(Dropout(params['dropout_hidden']))
+
+    # Flattening after the last CONV layer
+    model.add(Flatten())
+
+    # Number of nodes in output layer
+    model.add(Dense(num_labels, activation=params['output_activation']))
+
+    # Compiling the model to train. Declare the loss function and the optimizer (SGD, Adam, etc.)
+    model.compile(loss=loss, optimizer=params['optimizer'](learning_rate=params['learning_rate']),
+                  metrics=metrics)
+
+    return model
+
+
+def train(x_train: np.ndarray, y_train: np.ndarray, x_val: np.ndarray, y_val: np.ndarray, params: dict) -> Union[
+    Tuple[History, Sequential], str]:
+    # If list, then for tuning
+
+    if params['tuning']:
+        for_tuning, quiet = True, True
+    else:
+        for_tuning, quiet = False, False
+
+    if not quiet:
+        print()  # empty line after loading the data
+        print('Setting up the neural network')
+
+    # Define model architecture
+    if params['model_type'] == 'FC':  # fully connected
+        model = FC_model(x_train, params)
+    else:  # convolutional
+        model = CNN_model(x_train, params)
+
+    # visualise the model
+    # visualizer(model, filename='architecture', format='png', view=True)
+
+    # Train model
+    if not quiet:
+        print("Training...")
+
+    if val_portion > 0:
+        # Set early stopping monitor so the model stops training when it won't improve anymore
+        early_stopping_monitor = EarlyStopping(monitor='val_loss', patience=params['num_epochs'] / 3,
+                                               restore_best_weights=True)
+
+        history = model.fit(x_train, y_train, validation_data=(x_val, y_val), epochs=params['num_epochs'],
+                            batch_size=params['batch_size'], callbacks=[TerminateOnNaN(), early_stopping_monitor],
+                            verbose=verb)
+    else:
+        history = model.fit(x_train, y_train, validation_data=None, epochs=params['num_epochs'],
+                            batch_size=params['batch_size'], callbacks=[TerminateOnNaN()], verbose=verb)
+    if for_tuning:
+        return history, model
+
+    # Save model to project dir with a timestamp
+    now = datetime.now()
+    dt_string = now.strftime("%Y%m%d%H%M%S")
+    model_name = "".join((dt_string, '_', model_name_suffix, '.h5'))
+
+    filename = "".join((project_dir, '/Models/classification/', model_name))
+    check_dir(filename)
+    model.save(filename)
+
+    if not quiet:
+        print("Model was saved to disk")
+
+    if show_control_plot:
+        plot_model_history(model)
+
+    # Evaluate the train data
+    if not quiet:
+        acc = np.mean(metrics[0](y_train, model.predict(x_train)).numpy())
+        print('\nEvaluation on the data:')
+        print('Train accuracy:', str("{:7.5f}").format(np.round(acc, 5)))
+
+        if val_portion > 0:
+            # Evaluate the validation data
+            acc = np.mean(metrics[0](y_val, model.predict(x_val)).numpy())
+            print('Validation accuracy:', str("{:7.5f}").format(np.round(acc, 5)))
+        print()  # Empty line
+
+    return model_name
+
+
+def tune_hp(x_train: np.ndarray, y_train: np.ndarray, x_val: np.ndarray, y_val: np.ndarray) -> None:
+    scan_object = talos.Scan(x_train, y_train, params=p_for_tuning, model=train, x_val=x_val, y_val=y_val,
+                             experiment_name='tuning_HP')
+
+    best = scan_object.data.sort_values('val_' + metrics[0], ascending=False).iloc[0][-len(p_for_tuning):]
+    pprint(best)
---
+++ v1.0/modules/spectra_module.py
@@ -0,0 +1,232 @@
+import os
+from copy import deepcopy
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import pandas as pd
+from scipy.interpolate import interp1d
+from scipy.spatial import ConvexHull
+
+mpl.use('TkAgg')
+
+from modules.NN_data import load_data, clean_data
+from modules.utilities import *
+from modules.CD_parameters import *
+from modules.NN_config import num_labels_all
+
+
+def combine_files(filenames: Tuple[str, ...], final_name: str) -> str:
+    outfile_name = "".join((project_dir, '/Datasets/', final_name))
+    tmp = Path(outfile_name)
+    if tmp.suffix == '':
+        outfile_name += '.dat'
+    with open(outfile_name, 'w') as outfile:
+        for fname in filenames:
+            tmp = Path(fname)
+            if tmp.suffix == '':
+                fname += '.dat'
+            if Path(fname).parent.__str__() == '.':  # no directory; relab is default
+                with open("".join((project_dir, '/Datasets/RELAB/', fname)), 'r') as infile:
+                    for line in infile:
+                        outfile.write(line)
+
+            else:
+                with open(fname, 'r') as infile:
+                    for line in infile:
+                        outfile.write(line)
+    return outfile_name
+
+
+def clean_and_resave(filename: str, meta: bool = False) -> None:
+    # meta = True for resaving metadata
+    tmp = Path(filename)
+    if not meta:
+        final_name = tmp.stem + '-clean'  # '.dat' is added in save_data
+    else:
+        final_name = tmp.stem + '-clean-meta'
+
+    if not meta:
+        # open data file and clean it
+        spectra, labels = load_data(filename, clean_dataset=True, keep_all_labels=True)
+        # resave it (it is saved to relab folder...)
+        file = save_data(final_name, spectra=spectra, labels=labels)
+    else:
+        file = project_dir + '/Datasets/' + final_name + '.dat'
+        metadata = pd.read_csv(project_dir + '/Datasets/' + tmp.stem + '-meta.dat', sep='\t', header=None).to_numpy()
+
+        data_file = "".join((project_dir, '/Datasets/', filename))
+        data = np.loadtxt(data_file, delimiter='\t')
+
+        # Select training data
+        x_train, y_train = deepcopy(data[:, :-num_labels_all].astype(np.float32)), deepcopy(
+            data[:, -num_labels_all:].astype(np.float32))
+
+        *_, inds = clean_data(x_train, y_train, return_indices=True)
+
+        # filter metadata
+        metadata = metadata[inds]
+
+        with open(file, 'w') as f:
+            for element in metadata:
+                final_string = '\t'.join([str(e) for e in element]) + '\n'
+
+                for e in final_string:
+                    f.write(e)
+
+    # move it to Datasets folder
+    os.rename(file, project_dir + '/Datasets/' + final_name + '.dat')
+
+
+def denoise_and_norm(data: np.ndarray, denoising: bool, normalising: bool) -> np.ndarray:
+    xq = np.arange(lambda_min, lambda_max + resolution_final / 2, resolution_final)
+    if denoising:
+        width = 9
+        cent = int(np.round(width / 2))
+        kernel = np.zeros(width)
+
+        for ii in range(int(np.floor(width / 2))):
+            kernel[ii] = 1 / (np.abs(ii - cent) + 1)
+            kernel[-ii - 1] = 1 / (np.abs(ii - cent) + 1)
+        kernel[cent] = 1
+
+        kernel = kernel / np.sum(kernel)
+        correction = np.convolve(np.ones(len(xq)), kernel, 'same')
+
+        if data.ndim == 1:
+            data = np.reshape(data, (1, len(data)))
+        data_denoised = np.apply_along_axis(lambda m: np.convolve(m, kernel, 'same'), axis=1, arr=data) / correction
+    else:
+        data_denoised = data
+
+    # Normalised reflectance
+    if normalising:
+        fun = interp1d(xq, data_denoised, kind='cubic')  # v_final differs from v
+        v_norm = np.reshape(fun(normalised_at), (len(data_denoised), 1))
+    else:
+        v_norm = 1
+
+    return data_denoised / v_norm
+
+
+def save_data(final_name: str, spectra: np.ndarray, labels: np.ndarray = None, meta: np.ndarray = None,
+              Tuomas_data: bool = False) -> str:
+    check_dir("".join((path_relab, final_name, '.dat')))
+    if denoise:
+        if '-denoised' not in final_name:
+            final_name += '-denoised'
+    if normalise:
+        if '-norm' not in final_name:
+            final_name += '-norm'
+
+    if labels is None:
+        # Save data without labels (not needed for mixtures)
+        if not Tuomas_data:
+            filename = "".join((path_relab, final_name, '-nolabel.dat'))
+        else:
+            filename = "".join((path_tuomas, final_name, '-nolabel.dat'))
+
+        np.savetxt(filename, spectra, fmt='%.5f', delimiter='\t')
+
+    # save metadata if these exist
+    # data = pd.read_csv(filename, sep='\t', header=None).to_numpy()  # to read the file
+    if meta is not None:
+        filename = "".join((path_relab, final_name, '-meta.dat'))
+        with open(filename, 'w') as f:
+            for element in meta:
+                final_string = '\t'.join([str(e) for e in element]) + '\n'
+
+                for e in final_string:
+                    f.write(e)
+
+    # Save data with labels
+    if labels is not None:
+        if not Tuomas_data:
+            filename = "".join((path_relab, final_name, '.dat'))
+        else:
+            filename = "".join((path_tuomas, final_name, '.dat'))
+
+        if not Tuomas_data:
+            spectra = np.hstack((spectra, labels))
+            np.savetxt(filename, spectra, fmt='%.5f', delimiter='\t')
+        else:
+            spectra = np.hstack((labels, spectra))  # he stored labels in the first coordinate
+            np.savetxt(filename, spectra, fmt='%s', delimiter='\t')
+
+    return filename
+
+
+def normalize_spectra(file: str, save_it: bool = False) -> None:
+    path_to_data = path_relab
+
+    # load the data
+    data_file = "".join((path_to_data, '/', file, '.dat'))
+
+    xq = np.arange(lambda_min, lambda_max + resolution_final / 2, resolution_final)
+    data = np.loadtxt(data_file, delimiter='\t')
+
+    spectra, numbers = data[:, :-num_labels_CD], data[:, -num_labels_CD:]
+
+    fun = interp1d(xq, spectra, kind='cubic')
+    v_norm = fun(normalised_at)
+    spectra_final = np.transpose(np.divide(np.transpose(spectra), v_norm))
+
+    if save_it:
+        save_data(file, spectra_final, labels=numbers, Tuomas_data=False)
+
+
+def remove_continuum(modal: str) -> np.ndarray:
+    control_plot = False
+    n_labels = 15
+
+    input_file = "".join((path_relab, '/', modal, '-denoised.dat'))
+    output_file = "".join((path_relab, '/', modal, '-denoised-nocont_CH.dat'))
+
+    data = np.loadtxt(input_file, delimiter='\t')
+    spectra, numbers = data[:, :-n_labels], data[:, -n_labels:]
+
+    n_data, len_data = np.shape(spectra)
+    x = np.arange(lambda_min, lambda_max + resolution_final / 2, resolution_final)
+    rectified_spectra = np.zeros((n_data, len_data))
+
+    # 2D data for convex hull
+    ch_data = np.zeros((len_data, 2))
+    ch_data[:, 0] = x
+
+    for i in range(n_data):
+        spectrum = spectra[i]
+        ch_data[:, 1] = spectrum
+
+        hull = ConvexHull(ch_data).vertices
+
+        # remove lower branch from vertices (delete all vertices between 0 and len0data - 1
+        hull = np.roll(hull, -np.where(hull == 0)[0][0] - 1)  # move 0 to the end of the list
+        hull = np.sort(hull[np.where(hull == len_data - 1)[0][0]:])
+
+        # keep the UV bands
+        x0 = my_argmax(x, spectrum, x0=650)
+        hull = hull[np.argmin(np.abs(x[hull] - x0)):]
+        continuum = np.zeros(np.shape(x))  # necessary since the UVs start at different positions
+
+        # linear fit to the convex hull
+        for j in range(len(hull) - 1):
+            x_fit, y_fit = x[[hull[j], hull[j + 1]]], spectrum[[hull[j], hull[j + 1]]]
+            if j == 0 and hull[j] != 0:
+                x_new = x[:hull[j + 1] + 1]
+                continuum[:hull[j + 1] + 1] = np.polyval(np.polyfit(x_fit, y_fit, 1), x_new)
+            else:
+                x_new = x[hull[j]:hull[j + 1] + 1]
+                continuum[hull[j]:hull[j + 1] + 1] = np.polyval(np.polyfit(x_fit, y_fit, 1), x_new)
+
+        rectified_spectra[i] = spectrum / continuum
+        rectified_spectra = np.round(rectified_spectra, 5)
+
+        if control_plot:
+            fig, ax = plt.subplots()
+            ax.plot(x, spectrum / continuum)
+            ax.plot(x, spectrum)
+            ax.plot(x, continuum)
+
+    rectified_spectra = np.hstack((rectified_spectra, numbers))
+    np.savetxt(output_file, rectified_spectra, fmt='%.5f', delimiter='\t')
+
+    return rectified_spectra

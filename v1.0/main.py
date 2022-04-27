eaf0d7cdbb42a12639eb6b3a0f2ff0d7467937d0
Sirrah91 <92817370+Sirrah91@users.noreply.github.com>
2022-04-27 19:21:16 +0300

    Update main.py

--- v1.0/main.py
+++ v1.0/main.py
@@ -6,6 +6,20 @@
 # Install: numpy, tensorflow + dependencies, keras + dependencies         #
 # ----------------------------------------------------------------------- #
 
+"""
+Copyright 2022 David Korda, Antti Penttil, Arto Klami, Tomas Kohout (University of Helsinki and Institute of Geology of 
+the Czech Academy of Sciences). Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+software and associated documentation files (the 'Software'), to deal in the Software without restriction, including 
+without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of 
+the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: 
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of 
+the Software. 
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO 
+THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS 
+OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
 from os import environ
 environ['TF_CPP_MIN_LOG_LEVEL'] = "2"
 

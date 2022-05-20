9cef96f778c56e4f48f0269cd59237681ce013ac
Sirrah91 <92817370+Sirrah91@users.noreply.github.com>
2022-05-20 14:36:36 +0300

    Update main.py

--- v1.0/main.py
+++ v1.0/main.py
@@ -7,7 +7,7 @@
 # ----------------------------------------------------------------------- #
 
 """
-Copyright 2022 David Korda, Antti Penttil, Arto Klami, Tomas Kohout (University of Helsinki and Institute of Geology of 
+Copyright 2022 David Korda, Antti Penttila, Arto Klami, Tomas Kohout (University of Helsinki and Institute of Geology of 
 the Czech Academy of Sciences). Permission is hereby granted, free of charge, to any person obtaining a copy of this 
 software and associated documentation files (the 'Software'), to deal in the Software without restriction, including 
 without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of 

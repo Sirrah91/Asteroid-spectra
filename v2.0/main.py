6d0e9e63c1a11b23d2d6258db23170f798147188
Sirrah91 <92817370+Sirrah91@users.noreply.github.com>
2023-03-06 15:20:18 +0200

    Update main.py

--- v2.0/main.py
+++ v2.0/main.py
@@ -9,13 +9,13 @@
 """
 This code is provided under MIT license (https://opensource.org/license/mit/).
 
-Copyright 2023 David Korda, Tomáš Kohout, Kateřina Flanderová, Jean-Baptiste Vincent, and Antti Penttilä 
-(University of Helsinki, Institute of Geology of the Czech Academy of Sciences, and DLR Institute of Planetary Research).
+Copyright 2023 David Korda, Tomáš Kohout, Kateřina Flanderová, Jean-Baptiste Vincent, and Antti Penttilä (University of 
+Helsinki, Institute of Geology of the Czech Academy of Sciences, and DLR Institute of Planetary Research).
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this
-software and associated documentation files (the 'Software'), to deal in the Software without restriction, including
-without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+documentation files (the 'Software'), to deal in the Software without restriction, including without limitation 
+the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and 
+to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of
 the Software.
 

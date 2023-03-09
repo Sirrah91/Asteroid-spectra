f320f7c60d333766efef31be61a2ca9079af96d8
Sirrah91 <92817370+Sirrah91@users.noreply.github.com>
2023-03-10 00:18:54 +0200

    Update main_taxonomy.py

--- v2.0/main_taxonomy.py
+++ v2.0/main_taxonomy.py
@@ -7,7 +7,7 @@
 # ----------------------------------------------------------------------- #
 
 """
-This code is provided under MIT license (https://opensource.org/license/mit/).
+This code is provided under MIT licence (https://opensource.org/license/mit/).
 
 Copyright 2023 David Korda, Tomáš Kohout, Kateřina Flanderová, Jean-Baptiste Vincent, and Antti Penttilä (University of
 Helsinki, Institute of Geology of the Czech Academy of Sciences, and DLR Institute of Planetary Research).

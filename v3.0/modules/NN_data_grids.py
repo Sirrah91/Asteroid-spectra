b46a444b0226bed487a59860544648b8b89cd62e
Sirrah <92817370+Sirrah91@users.noreply.github.com>
2023-09-19 19:44:16 +0300

    Update NN_data_grids.py

--- v3.0/modules/NN_data_grids.py
+++ v3.0/modules/NN_data_grids.py
@@ -2,10 +2,11 @@ import numpy as np
 from math import isclose
 import warnings
 
+# defaults only
 from modules._constants import _num_eps
 
 
-def check_grid(grid: np.ndarray) -> None:
+def check_grid(grid: np.ndarray, num_eps: float = _num_eps) -> None:
     if grid[1] < grid[0]:
         raise ValueError("Invalid grid. Ending wavelength is not larger than starting one.")
 
@@ -13,8 +14,8 @@ def check_grid(grid: np.ndarray) -> None:
         raise ValueError("Invalid grid. Normalisation is not within the grid range.")
 
     grid_resolution_fraction = np.mod((grid[1] - grid[0]) / grid[2], 1.0)
-    if  not (isclose(grid_resolution_fraction, 0.0, abs_tol=_num_eps) or
-             isclose(grid_resolution_fraction, 1.0, abs_tol=_num_eps)):
+    if  not (isclose(grid_resolution_fraction, 0.0, abs_tol=num_eps) or
+             isclose(grid_resolution_fraction, 1.0, abs_tol=num_eps)):
         warnings.warn("Warning. The grid may have incorrect resolution or limits.")
 
 

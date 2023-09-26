32ff1246c7e47e7cf6abcf53d88fb535e7fe3c9e
Sirrah <92817370+Sirrah91@users.noreply.github.com>
2023-09-26 12:11:14 +0300

    Update NN_config_taxonomy.py

--- v3.0/modules/NN_config_taxonomy.py
+++ v3.0/modules/NN_config_taxonomy.py
@@ -30,7 +30,7 @@ tax_model_setup = {
     "metrics": ["f1_score"],  # must be in custom_objects in custom_objects in NN_losses_metrics_activations.py
 
     # important for HP tuning and early stopping
-    "monitoring": {"objective": "val_f1_score",  # if is not loss, must be included in custom_objects
+    "monitoring": {"objective": "val_f1_score",  # if is not loss, must be included in custom_objects and metrics
                    "direction": "max"  # minimise or maximise the objective (for HP tuning)?
                    },
 

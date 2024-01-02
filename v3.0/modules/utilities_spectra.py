dce8822fd9d345221de9602cf2dfedc60682954b
Sirrah <92817370+Sirrah91@users.noreply.github.com>
2024-01-02 12:51:18 +0200

    Update utilities_spectra.py

--- v3.0/modules/utilities_spectra.py
+++ v3.0/modules/utilities_spectra.py
@@ -386,7 +386,11 @@ def apply_transmission(spectra: np.ndarray,
     if sum_or_int == "sum":
         final_spectra = spectra @ np.transpose(transmission)
     else:
-        final_spectra = trapezoid(y=np.einsum('ij, kj -> ikj', spectra, transmission), x=wvl_transmission)
+        if np.ndim(spectra) == 1:
+            final_spectra = trapezoid(y=spectra * transmission, x=wvl_transmission)
+        else:
+            final_spectra = trapezoid(y=np.einsum('...ij, ...kj -> ...ikj', spectra, transmission), x=wvl_transmission)
+        
 
     wvl_central, final_spectra = np.array(wvl_central, dtype=_wp), np.array(final_spectra, dtype=_wp)
 

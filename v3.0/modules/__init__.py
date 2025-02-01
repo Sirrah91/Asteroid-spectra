9c82bbc3bfab04d0e371497373223168bca8bc1b
Sirrah <92817370+Sirrah91@users.noreply.github.com>
2025-02-01 12:19:50 +0100

    Update __init__.py

--- v3.0/modules/__init__.py
+++ v3.0/modules/__init__.py
@@ -1,19 +1,1 @@
-"""
-WEB APP
-- upload form
-    - secure open
-    - nejak dostat dalsi requirements tak, aby nesel odeslat formular
-    - nejak udelat, aby se nevynulvoval formular po vykresleni obrazku (resubmit form nebo update image without refresh)
 
-- PC graf
-    - neni hezke, ze to je v /upload a ne v /summary
-
-- grid
-    - jake modely vytvorit?
-        - zacit typickymi rozlisenimi dalekohledu
-
------------------------------------------------------
-
-TO DO LIST
-- pridat realisticky sum pro aspect
-"""
